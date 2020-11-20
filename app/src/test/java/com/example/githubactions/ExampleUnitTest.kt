aaaf20d662c943a3544f7b569080c3e80690dd72
David Gyavol <david.gyavol@fortech.ro>
2020-11-20 13:51:41 +0200

    Fix tests

--- app/src/test/java/com/example/githubactions/ExampleUnitTest.kt
+++ app/src/test/java/com/example/githubactions/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.example.githubactions
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,10 +12,5 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
-    }
-
-    @Test
-    fun addition_isIncorrect() {
-        assertEquals(5, 2 + 2)
     }
 }
